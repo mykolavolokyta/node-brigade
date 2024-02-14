5d119306f6ace42a08f220edb4a42bd607fe74d8
Vladyslav D <donetsvlad444@gmail.com>
2024-02-14 10:45:24 +0200

    set routes, controllers and ejs

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,24 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const path = require('path');
+const path = require("path");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '/../views/index.html'));
+const UserController = require("../controllers/userController");
+
+// home route
+router.get("/", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "/../views/index.html"));
 });
+
+// vlad route
+router.get("/vlad", UserController.vladController);
+
+// kolya route
+router.get("/kolya", UserController.kolyaController);
+
+// ruslan route
+router.get("/ruslan", UserController.ruslanController);
+
+// max route
+router.get("/max", UserController.maxController);
 
 module.exports = router;
