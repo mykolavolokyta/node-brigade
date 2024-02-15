4fd96fb3d16324668b7d03e2558bd37518da084f
MaxTereshchuk <namestar.max@gmail.com>
2024-02-15 14:38:32 +0200

    add max information

--- controllers/userController.js
+++ controllers/userController.js
@@ -44,11 +44,10 @@ class UserController {
   static maxController = (req, res, next) => {
     res.render("../views/user", {
       userName: "Max",
-      userDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-      when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-      It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`,
-      userImage: "vlad.jpg",
+      userDescription: `<br>Студент групи ІП-23<br>
+      Терещук Максим Русланович<br> 
+      Фанат Динамо Київ`,
+      userImage: "/images/max.jpg",
       github: "https://github.com/maxtereshchuk"
     });
   };
